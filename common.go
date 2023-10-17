6b581fe5102a849f76c0965f25d7e5e11498d6da
ReyRen <reyren@163.com>
2023-10-17 19:03:10 +0800

    重新写了 docker 的方法，并且能够成功从拉取镜像到容器启动，将 gpu 个数以及端口映射，存储映射都完成了

--- common.go
+++ common.go
@@ -4,7 +4,10 @@ import "strings"
 
 var (
 	RPC_ADDDR_AND_PORT = "172.18.127.62:50001"
-	//RPC_REGISTER_NAME = "GCSInfoCatchService"
+
+	DOCKER_REGISTRY_USERNAME = "admin"
+	DOCKER_REGISTRY_PASSWORD = "adminADMIN123"
+	DOCKER_REGISTRY_ADDRESS  = "http://172.18.127.68:80"
 )
 
 // GCSInfoCatchServer is rpc server obj
--- docker_system.go
+++ docker_system.go
@@ -3,37 +3,61 @@ package main
 import (
 	pb "GCS-Info-Catch/proto"
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/api/types/registry"
 	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/go-connections/nat"
 	"io"
 	"log"
-	"os"
+	"time"
 )
 
-func (g *GCSInfoCatchServer) DockerContainerImagePull(req *pb.ImagePullRequestMsg, stream pb.GcsInfoCatchServiceDocker_DockerContainerImagePullServer) error {
+func (g *GCSInfoCatchServer) DockerContainerRun(req *pb.ContainerRunRequestMsg,
+	stream pb.GcsInfoCatchServiceDocker_DockerContainerRunServer) error {
+
+	log.Printf("docker container run:[%v][%v][%v]\n", req.GetContainerName(), req.GetGpuIdx(), req.GetImageName())
+
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		log.Printf("NewClientWithOpts Error:", err.Error())
+		return err
 	}
 	defer cli.Close()
 	var out io.ReadCloser
 	// 创建一个缓冲区来保存数据
 	buf := make([]byte, 4096)
 
-	for {
-		out, err = cli.ImagePull(ctx, req.GetImageName(), types.ImagePullOptions{})
+	authConfig := registry.AuthConfig{
+		Username:      DOCKER_REGISTRY_USERNAME,
+		Password:      DOCKER_REGISTRY_PASSWORD,
+		ServerAddress: DOCKER_REGISTRY_ADDRESS,
+	}
+	encodedJSON, err := json.Marshal(authConfig)
+	if err != nil {
+		log.Printf("auth registry Marshal error:%v\n", err.Error())
+		return err
+	}
+	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+
+	log.Println("image pull start")
+
+	out, err = cli.ImagePull(ctx, req.GetImageName(), types.ImagePullOptions{RegistryAuth: authStr})
+	if err != nil {
+		log.Printf("cli.ImagePull Error:", err.Error())
+		err = stream.Send(&pb.ContainerRunRespondMsg{RunResp: "IMAGE_ERROR"})
 		if err != nil {
-			log.Printf("cli.ImagePull Error:", err.Error())
-			err = stream.Send(&pb.ImagePullRespondMsg{ImageResp: "image pull get error " + err.Error()})
-			if err != nil {
-				log.Printf("Stream send error:%v", err.Error())
-				return err
-			}
+			log.Printf("Stream send error:%v", err.Error())
 			return err
 		}
+		return err
+	}
+	defer out.Close()
+	for {
 		n, err := out.Read(buf)
 		if err != nil {
 			if err != io.EOF {
@@ -41,215 +65,308 @@ func (g *GCSInfoCatchServer) DockerContainerImagePull(req *pb.ImagePullRequestMs
 				return err
 			}
 			// EOF
+			log.Println("out.Read EOF occurred")
+			out.Close()
 			break
 		}
-		err = stream.Send(&pb.ImagePullRespondMsg{ImageResp: string(buf[:n])})
+		time.Sleep(2 * time.Second)
+		log.Printf("%v\n", string(buf[:n]))
+		err = stream.Send(&pb.ContainerRunRespondMsg{RunResp: "IMAGE_PULLING"})
 		if err != nil {
 			log.Printf("Stream send error:%v", err)
 			return err
 		}
 	}
-	defer out.Close()
 
-	return nil
-}
-
-func (g *GCSInfoCatchServer) DockerContainerStart(req *pb.StartRequestMsg, stream pb.GcsInfoCatchServiceDocker_DockerContainerStartServer) error {
-	ctx := context.Background()
-
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	//开始创建容器
+	err = stream.Send(&pb.ContainerRunRespondMsg{RunResp: "CONTAINER_CREATING"})
 	if err != nil {
-		//*reply = "client.NewClientWithOpts get Error:" + request
-		panic(err)
+		log.Printf("Stream send error:%v", err)
+		return err
+	}
+	log.Println("container create start")
+	exposedPorts := nat.PortSet{nat.Port("22/tcp"): {}}
+	portBindings := []nat.PortBinding{{
+		HostIP:   "",
+		HostPort: "50022",
+	}}
+
+	capabilities := [][]string{{"gpu"}}
+	var deviceIDs []string
+	deviceRequest := []container.DeviceRequest{
+		{
+			DeviceIDs:    append(deviceIDs, req.GetGpuIdx()),
+			Capabilities: capabilities,
+		},
 	}
 
-	defer cli.Close()
-
-	imageName := "request"
-	out, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
-	if err != nil {
-		panic(err)
+	var mountVolume []mount.Mount
+	mountData := mount.Mount{
+		Type:     "bind",
+		Source:   "/storage-ftp-data",
+		Target:   "/storage-root",
+		ReadOnly: false,
 	}
-	defer out.Close()
-	io.Copy(os.Stdout, out)
 
+	portMaps := nat.PortMap{nat.Port("22/tcp"): portBindings}
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
 		User:         "root",
-		ExposedPorts: nil,
+		ExposedPorts: exposedPorts,
 		Env:          nil,
-		Healthcheck:  nil,
-		Image:        imageName,
-		Volumes:      nil,
-		WorkingDir:   "",
-		//覆盖镜像中的 entrypoint 脚本，防止镜像自动退出
-		Entrypoint:  []string{"/bin/bash", "-c", "tail -f /dev/null"},
-		Labels:      nil,
-		StopSignal:  "",
-		StopTimeout: nil,
+		Image:        req.GetImageName(),
+		Entrypoint:   []string{"/bin/bash", "-c", "tail -f /dev/null"},
 	}, &container.HostConfig{
 		Binds:           nil,
 		LogConfig:       container.LogConfig{},
 		NetworkMode:     "",
-		PortBindings:    nil,
+		PortBindings:    portMaps,
 		RestartPolicy:   container.RestartPolicy{},
-		AutoRemove:      false,
-		VolumeDriver:    "",
-		VolumesFrom:     nil,
-		ConsoleSize:     [2]uint{},
-		Annotations:     nil,
-		CapAdd:          nil,
-		CapDrop:         nil,
-		CgroupnsMode:    "",
-		DNS:             nil,
-		DNSOptions:      nil,
-		DNSSearch:       nil,
-		ExtraHosts:      nil,
-		GroupAdd:        nil,
+		AutoRemove:      true,
 		IpcMode:         container.IPCModeHost,
-		Cgroup:          "",
-		Links:           nil,
-		OomScoreAdj:     0,
-		PidMode:         "",
-		Privileged:      true,
+		Privileged:      false,
 		PublishAllPorts: false,
-		ReadonlyRootfs:  false,
-		SecurityOpt:     nil,
-		StorageOpt:      nil,
-		Tmpfs:           nil,
-		UTSMode:         "",
-		UsernsMode:      "",
-		ShmSize:         128,
+		ShmSize:         512,
 		Resources: container.Resources{
-			CPUShares:            0,
-			Memory:               0,
-			NanoCPUs:             0,
-			CgroupParent:         "",
-			BlkioWeight:          0,
-			BlkioWeightDevice:    nil,
-			BlkioDeviceReadBps:   nil,
-			BlkioDeviceWriteBps:  nil,
-			BlkioDeviceReadIOps:  nil,
-			BlkioDeviceWriteIOps: nil,
-			CPUPeriod:            0,
-			CPUQuota:             0,
-			CPURealtimePeriod:    0,
-			CPURealtimeRuntime:   0,
-			CpusetCpus:           "",
-			CpusetMems:           "",
-			//Devices:              device,
-			DeviceCgroupRules: nil,
-			//DeviceRequests:     deviceRequest, // 这里增加 GPU
-			KernelMemoryTCP:    0,
-			MemoryReservation:  0,
-			MemorySwap:         0,
-			MemorySwappiness:   nil,
-			OomKillDisable:     nil,
-			PidsLimit:          nil,
-			Ulimits:            nil,
-			CPUCount:           0,
-			CPUPercent:         0,
-			IOMaximumIOps:      0,
-			IOMaximumBandwidth: 0,
+			DeviceRequests: deviceRequest,
 		},
-		Mounts:        nil,
-		MaskedPaths:   nil,
-		ReadonlyPaths: nil,
-		Init:          nil,
-	}, nil, nil, "")
-	if err != nil {
-		panic(err)
-	}
+		Mounts: append(mountVolume, mountData),
+	}, nil, nil, req.GetContainerName())
 
 	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-		panic(err)
+		log.Printf("container start error:%v\n", err.Error())
+		return err
 	}
+	log.Println("container started")
 
-	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-	select {
-	case err := <-errCh:
-		if err != nil {
-			panic(err)
+	ticker := time.NewTicker(500 * time.Millisecond)
+	for {
+		select {
+		case <-ticker.C:
+			status, err := containerStatus(cli, ctx, resp.ID)
+			if err != nil {
+				log.Printf("containerStatus get error:%v\n", err.Error())
+				return err
+			}
+			if status == "" {
+				err = stream.Send(&pb.ContainerRunRespondMsg{RunResp: "CONTAINER_REMOVED"})
+				if err != nil {
+					log.Printf("Stream send error:%v", err)
+				}
+				log.Printf("containerID not found\n")
+				return err
+			}
+			if status == "running" {
+				err = stream.Send(&pb.ContainerRunRespondMsg{RunResp: "CONTAINER_RUNNING"})
+				if err != nil {
+					log.Printf("Stream send error:%v", err)
+				}
+				log.Printf("container running:%v\n", resp.ID)
+				return err
+
+			}
+			if status == "created" {
+				err = stream.Send(&pb.ContainerRunRespondMsg{RunResp: "CONTAINER_CREATED"})
+				if err != nil {
+					log.Printf("Stream send error:%v", err)
+				}
+				log.Printf("container created:%v\n", resp.ID)
+				return err
+			}
+			if status == "exited" {
+				err = stream.Send(&pb.ContainerRunRespondMsg{RunResp: "CONTAINER_EXITED"})
+				if err != nil {
+					log.Printf("Stream send error:%v", err)
+				}
+				log.Printf("container exited:%v\n", resp.ID)
+				return err
+			}
+			if status == "dead" {
+				err = stream.Send(&pb.ContainerRunRespondMsg{RunResp: "CONTAINER_DEAD"})
+				if err != nil {
+					log.Printf("Stream send error:%v", err)
+				}
+				log.Printf("container dead:%v\n", resp.ID)
+				return err
+			}
+			if status == "removing" {
+				err = stream.Send(&pb.ContainerRunRespondMsg{RunResp: "CONTAINER_REMOVING"})
+				if err != nil {
+					log.Printf("Stream send error:%v", err)
+				}
+				log.Printf("container removing:%v\n", resp.ID)
+				return err
+			}
+			if status == "restarting" {
+				err = stream.Send(&pb.ContainerRunRespondMsg{RunResp: "CONTAINER_RESTARTING"})
+				if err != nil {
+					log.Printf("Stream send error:%v", err)
+				}
+				log.Printf("container restarting:%v\n", resp.ID)
+				return err
+			}
 		}
-	case <-statusCh:
 	}
+}
 
-	out, err = cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+// containerStatus returns one of "created", "running", "paused", "restarting", "removing", "exited", or "dead"
+// will return nil if containerID is not found
+func containerStatus(c *client.Client, ctx context.Context, containerID string) (string, error) {
+	ci, err := c.ContainerInspect(ctx, containerID)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
-
-	return nil
+	return ci.ContainerJSONBase.State.Status, nil
 }
 
 func (g *GCSInfoCatchServer) DockerContainerDelete(req *pb.DeleteRequestMsg, stream pb.GcsInfoCatchServiceDocker_DockerContainerDeleteServer) error {
+	log.Printf("docker container delete:[%v]\n", req.GetContainName())
+
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Printf("NewClientWithOpts Error:", err.Error())
+		return err
+	}
+
+	err = cli.ContainerRemove(ctx, req.GetContainName(), types.ContainerRemoveOptions{
+		Force: true,
+	})
+	if err != nil {
+		log.Printf("ContainerRemove Error:", err.Error())
+		return err
+	}
 	return nil
 }
 
 func (g *GCSInfoCatchServer) DockerContainerStatus(req *pb.StatusRequestMsg, stream pb.GcsInfoCatchServiceDocker_DockerContainerStatusServer) error {
-	/*swarm, err := cli.SwarmInspect(ctx)
-	//slog.Debug("swarm information", "SWARM_JoinTokens", swarm.JoinTokens.Manager)
-	slog.Debug("swarm information", "SWARM_JoinTokens", swarm.ClusterInfo.)
-	*/
-	/*swarmNodes, err := cli.NodeList(ctx, types.NodeListOptions{})
-	for _, v := range swarmNodes {
-		slog.Debug("swarm information", "HOSTNAME", v.Description.Hostname)
-		slog.Debug("swarm information", "STATUS", v.Status)
-		slog.Debug("swarm information", "Availability", v.Spec.Availability)
-		slog.Debug("swarm information", "Role", v.Spec.Role)
-		slog.Debug("swarm information", "Annotation_name", v.Spec.Annotations.Name)
-	}*/
+	log.Printf("docker container status:[%v]\n", req.GetContainerName())
 
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Printf("NewClientWithOpts Error:", err.Error())
+		return err
+	}
+
+	inspect, err := cli.ContainerInspect(ctx, req.GetContainerName())
+	if err != nil {
+		return err
+	}
+	status := inspect.State.Status
+	if err != nil {
+		log.Printf("containerStatus get error:%v\n", err.Error())
+		return err
+	}
+	if status == "" {
+		log.Printf("containerID not found\n")
+		err = stream.Send(&pb.StatusRespondMsg{StatusResp: "CONTAINER_REMOVED"})
+		if err != nil {
+			log.Printf("Stream send error:%v", err)
+			return err
+		}
+	}
+	if status == "running" {
+		log.Printf("container running:%v\n", req.GetContainerName())
+		err = stream.Send(&pb.StatusRespondMsg{StatusResp: "CONTAINER_RUNNING"})
+		if err != nil {
+			log.Printf("Stream send error:%v", err)
+			return err
+		}
+	}
+	if status == "created" {
+		err = stream.Send(&pb.StatusRespondMsg{StatusResp: "CONTAINER_CREATED"})
+		if err != nil {
+			log.Printf("Stream send error:%v", err)
+			return err
+		}
+		log.Printf("container created:%v\n", req.GetContainerName())
+	}
+	if status == "exited" {
+		err = stream.Send(&pb.StatusRespondMsg{StatusResp: "CONTAINER_EXITED"})
+		if err != nil {
+			log.Printf("Stream send error:%v", err)
+			return err
+		}
+		log.Printf("container exited:%v\n", req.GetContainerName())
+	}
+	if status == "dead" {
+		err = stream.Send(&pb.StatusRespondMsg{StatusResp: "CONTAINER_DEAD"})
+		if err != nil {
+			log.Printf("Stream send error:%v", err)
+			return err
+		}
+		log.Printf("container dead:%v\n", req.GetContainerName())
+	}
+	if status == "removing" {
+		err = stream.Send(&pb.StatusRespondMsg{StatusResp: "CONTAINER_REMOVING"})
+		if err != nil {
+			log.Printf("Stream send error:%v", err)
+			return err
+		}
+		log.Printf("container removing:%v\n", req.GetContainerName())
+	}
+	if status == "restarting" {
+		err = stream.Send(&pb.StatusRespondMsg{StatusResp: "CONTAINER_RESTARTING"})
+		if err != nil {
+			log.Printf("Stream send error:%v", err)
+			return err
+		}
+		log.Printf("container restarting:%v\n", req.GetContainerName())
+	}
 	return nil
 }
 
 func (g *GCSInfoCatchServer) DockerContainerLogs(req *pb.LogsRequestMsg, stream pb.GcsInfoCatchServiceDocker_DockerContainerLogsServer) error {
-	/*cli.ContainerLogs(ctx, "name", types.ContainerLogsOptions{
-		ShowStdout: false,
-		ShowStderr: false,
-		Since:      "",
-		Until:      "",
-		Timestamps: false,
-		Follow:     false,
-		Tail:       "",
-		Details:    false,
-	})*/
+	log.Printf("docker container log:[%v]\n", req.GetContainerName())
 
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Printf("NewClientWithOpts Error:", err.Error())
+		return err
+	}
+	defer cli.Close()
+	var out io.ReadCloser
+	defer out.Close()
+	// 创建一个缓冲区来保存数据
+	buf := make([]byte, 4096)
+	for {
+		out, err := cli.ContainerLogs(ctx, req.GetContainerName(), types.ContainerLogsOptions{
+			ShowStdout: false,
+			ShowStderr: false,
+			Since:      "",
+			Until:      "",
+			Timestamps: false,
+			Follow:     true,
+			Tail:       "",
+			Details:    false,
+		})
+		if err != nil {
+			log.Printf("cli.ContainerLogs Error:", err.Error())
+			err = stream.Send(&pb.LogsRespondMsg{LogsResp: "LOGS_ERROR"})
+			if err != nil {
+				log.Printf("Stream send error:%v", err.Error())
+				return err
+			}
+			return err
+		}
+
+		n, err := out.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("out.Read error occurred: %v", err.Error())
+				return err
+			}
+			// EOF
+			log.Println("out.Read EOF occurred")
+			break
+		}
+		log.Printf("%v\n", buf[:n])
+		err = stream.Send(&pb.LogsRespondMsg{LogsResp: string(buf[:n])})
+		if err != nil {
+			log.Printf("Stream send error:%v", err)
+			return err
+		}
+	}
 	return nil
 }
-
-/*device := []container.DeviceMapping{
-	{
-		PathOnHost:        "/dev/infiniband/uverbs0",
-		PathInContainer:   "",
-		CgroupPermissions: "",
-	},
-	{
-		PathOnHost:        "/dev/infiniband/uverbs1",
-		PathInContainer:   "",
-		CgroupPermissions: "",
-	},
-}*/
-//capabilities := [][]string{{"gpu"}}
-//deviceIds := []string{"GPU-3a335d70-3a17-6e18-bd16-5643a1d2c0ae", "GPU-1413bca2-67df-b468-5d72-3d26dbe12205"}
-/*deviceRequest := []container.DeviceRequest{
-	{
-		Driver: "",
-		Count:  2,
-		//DeviceIDs:    deviceIds,
-		Capabilities: capabilities,
-		//Options:      {"", ""},
-	},
-}*/
-/*device := []container.DeviceMapping{
-	{
-		PathOnHost:        "/dev/infiniband/uverbs0",
-		PathInContainer:   "",
-		CgroupPermissions: "",
-	},
-	{
-		PathOnHost:        "/dev/infiniband/uverbs1",
-		PathInContainer:   "",
-		CgroupPermissions: "",
-	},
-}*/
